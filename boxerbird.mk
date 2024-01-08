bdb03dc4d06776e7035ca99a030b6237503aa797
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-08 10:56:13 -0600

    created standard entry point boxerbird.mk

---
+++ boxerbird.mk
@@ -0,0 +1,4 @@
+BOXERBIRD_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+include $(BOXERBIRD_PATH)/src/bash-build-tools.mk
+include $(BOXERBIRD_PATH)/src/common-targets.mk
+include $(BOXERBIRD_PATH)/src/install-tools.mk
