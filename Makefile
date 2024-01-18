724e59f8a2b3095380a8ecbb5dc8fbbca8365b6f
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-18 14:45:36 -0600

    added help command to Makefile

--- Makefile
+++ Makefile
@@ -2,13 +2,18 @@
 WORKDIR_ROOT := $(CURDIR)/.make
 
 #Targets
+Default: help
+
 .PHONY: check
+## Runs the repository tests
 check: private_test
 
 .PHONY: clean
+## Deletes all files created by Make
 clean: private_clean
 
 .PHONY: test
+## Runs the repository tests
 test: private_test
 
 # Includes
--- src/common-targets.mk
+++ src/common-targets.mk
@@ -1,3 +1,20 @@
+.PHONY: help
+## Provides this help message
+help:
+	@echo "Available targets:"
+	@awk '/^[a-zA-Z\-\_0-9%:\\]+/ { \
+		helpMessage = match(lastLine, /^## (.*)/); \
+		if (helpMessage) { \
+			helpCommand = $$1; \
+			helpMessage = substr(lastLine, RSTART + 3, RLENGTH); \
+			gsub("\\\\", "", helpCommand); \
+			gsub(":+$$", "", helpCommand); \
+			printf "   %-23s %s\n", helpCommand, helpMessage; \
+		} \
+	} \
+	{ lastLine = $$0 }' $(MAKEFILE_LIST) | sort -u
+	@echo
+
 .PRECIOUS: %/.
 %/. :
 	@mkdir -p $@
